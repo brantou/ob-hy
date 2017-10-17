57eece4cac972759b37b775799ddebcd67882ab5
brantou <brantou89@gmail.com>
2017-10-17 23:16:44 +0800

    add package desc

--- ob-hy.el
+++ ob-hy.el
@@ -38,6 +38,9 @@
 ;;
 ;; - hy :: https://hy-lang.org/
 ;;
+;; - hy-mode :: Can be installed through from ELPA, or from
+;;   https://raw.githubusercontent.com/hylang/hy-mode/master/hy-mode.el
+;;
 
 ;;; TODO
 ;;
